c075ae27435c5ffab048fbff30cab9c17d57a4db
code <mmahad280@gmail.com>
2020-08-25 19:31:05 -0400

    added all material ui icons to searcbaroptions.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,32 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption.js";
+
+// All material ui icon imports
+
+import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 function Sidebar() {
 	return (
 		<div>
-			<h1>Hello</h1>
-			{/* twitter icon */}
-
-			{/* sidebar option component */}
-			{/* sidebar option component */}
+			<h1>Sidebar</h1>
+			<TwitterIcon />
+			<SidebarOption Icon={HomeIcon} text="Home" />
+			<SidebarOption Icon={SearchIcon} text="Explore" />
+			<SidebarOption Icon={NotificationsIcon} text="Notifications" />
+			<SidebarOption Icon={MailOutlineIcon} text="Messages" />
+			<SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+			<SidebarOption Icon={ListAltIcon} text="Lists" />
+			<SidebarOption Icon={PermIdentityIcon} text="Profile" />
+			<SidebarOption Icon={MoreHorizIcon} text="more" />
 		</div>
 	);
 }
