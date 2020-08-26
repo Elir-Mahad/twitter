a68eb536bfbd99db9b12f7ea3f1325ee99782459
code <mmahad280@gmail.com>
2020-08-25 23:59:13 -0400

    added post component

---
+++ src/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Post.css";
+
+function Post() {
+	//! ALL USESTATES BELOW
+
+	return (
+		<div className="post">
+			<h1>Post</h1>
+		</div>
+	);
+}
+
+export default Post;
